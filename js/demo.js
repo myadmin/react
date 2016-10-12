d0bdf5192bb1ec069b871438b36db9a7a21110c5
zhouwei <zhou21192345@163.com>
2016-10-12 17:48:21 +0800

    add js

---
+++ js/demo.js
@@ -0,0 +1,38 @@
+var SetIntervalMixins = {
+	componentWillMount: function () {
+		this.intervals = [];
+	},
+	setInterval: function () {
+		this.intervals.push(setInterval.apply(null, arguments));
+	},
+	componentWillUnmount: function () {
+		this.intervals.map(clearInterval);
+	}
+};
+
+var TickTok = React.createClass({
+	mixins: [SetIntervalMixins],
+	getInitialState: function () {
+		return {
+			seconds: 0
+		}
+	},
+	componentDidMount: function () {
+		this.setInterval(this.tick, 1000);
+	},
+	tick: function () {
+		this.setState({
+			seconds: this.state.seconds + 1
+		})
+	},
+	render: function () {
+		return (
+			<p>React han been running for {this.state.seconds} seconds.</p>
+		);
+	}
+});
+
+var dom = ReactDOM.render(
+	<TickTok />,
+	document.getElementById("app")
+);
